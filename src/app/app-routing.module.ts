54bfe28badc0153eaf81c527b0ce5170402b2164
Jesus David Olmos <jesusdavidolmos@hotmail.com>
2021-09-27 12:20:30 -0500

    Registro de usuario finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
 import { SetProductosComponent } from './backend/set-productos/set-productos.component';
+import { PerfilComponent } from './page/perfil/perfil.component';
 
 const routes: Routes = [
   {
@@ -10,6 +11,7 @@ const routes: Routes = [
   {
     path: 'set-productos', component: SetProductosComponent
   },
+  { path: 'perfil', component: PerfilComponent },
   {
     path: '', component: HomeComponent
   },
